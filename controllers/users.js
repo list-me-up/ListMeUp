c5cfb29424d58bebd20d9d3bc20f21b39737608c
incadenza <andrewbdell@me.com>
2017-08-11 16:20:36 -0700

    user location, time and phone number

--- controllers/users.js
+++ controllers/users.js
@@ -1,14 +1,17 @@
 var User = require('../models/user');
+var geocoder = require('geocoder');
 
 function index(req, res) {
     var u = req.user;
 
     if (u.lat && u.lon && u.phoneNumber && u.time) {
         res.redirect('users/home');
+        console.log(req.body)
         // load to to-do list
     } else {
         // redirect to profile
         res.render('users/settings');
+        console.log(req.body)        
     }
 }
 
@@ -16,6 +19,18 @@ function home(req, res) {
 
 }
 
+function update(req, res) {
+    req.user.phoneNumber = req.body.phoneNumber
+    req.user.time = req.body.time
+  
+    geocoder.geocode(req.body.city, function (err, data) {
+        req.user.lat = data.results[0].geometry.location.lat
+        req.user.lon = data.results[0].geometry.location.lng
+        req.user.save(function(err) {
+            res.redirect('/');
+        });
+    });
+}
 function settings(req, res) {
     res.render('users/home');
 }
@@ -33,5 +48,6 @@ module.exports = {
     index,
     settings,
     newMap,
-    home
+    home,
+    update,
 }
--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,7 @@ router.get('/profile', userCtrl.index);
 router.get('/home', userCtrl.home)
 router.get('/settings', userCtrl.settings);
 router.get('/new', userCtrl.newMap);
+router.put('/profile', userCtrl.update);
 
 // user settings
 
