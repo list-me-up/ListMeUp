932b21e70366dc73af8c87c3983134ce00f20e98
incadenza <andrewbdell@me.com>
2017-08-14 09:17:15 -0700

    add first version of twilio instance method

--- models/user.js
+++ models/user.js
@@ -1,5 +1,13 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+require('dotenv').config();
+
+const token = process.env.TWILIO_TOKEN;
+const accountSid = process.env.TWILIO_ACCOUNT;
+const telephone = process.env.TWILIO_NUMBER
+
+const twilio = require('twilio')(accountSid, token);
 
 var listSchema = new Schema({
     text: String,
@@ -19,5 +27,19 @@ var userSchema = new Schema({
 }, {
     timestamps: true
 });
+
+userSchema.methods.sendMessage = function() {
+    twilio.messages.create({
+        to: `+1${this.phoneNumber}`,
+        from: telephone,
+        body: `Hello, ${this.name}`,
+    }, function (err, message) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(message.sid);
+        }
+    });
+}
 
 module.exports = mongoose.model('User', userSchema);
