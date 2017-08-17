dee617f82ab7a00bb67678915275f32bce2d6b0e
incadenza <andrewbdell@me.com>
2017-08-17 13:13:43 -0700

    add current weather instance method

--- config/notifications.js
+++ config/notifications.js
@@ -1,6 +1,14 @@
 const User = require('../models/user');
 const schedule = require('node-schedule');
 
+schedule.scheduledJob('00 * * * *', function () {
+  User.find({}, function(error, users) {
+    users.forEach(function(user) {
+      if (user.weatherLocation) user.getCurrentWeather().then(weather => { user.currentWeather = weather })
+    })
+  })
+})
+
 schedule.scheduleJob('0 * * * * *', function () {
   let currentTime = new Date()
   currentTime = currentTime.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
